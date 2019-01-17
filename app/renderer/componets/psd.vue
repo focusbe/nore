7014d57d26b572e777660d406a58e63b52224a70
liupeng <liupeng@ztgame.com>
2019-01-17 14:03:15 +0800

    同步代码

--- app/renderer/componets/psd.vue
+++ app/renderer/componets/psd.vue
@@ -78,7 +78,7 @@ export default {
 					this.uploadpath,
 					this.uploadpath
 				);
-				var res = await mypsd.parse(false, true);
+				var res = await mypsd.parse(true, true);
 				console.log(res);
 			} catch (error) {
 				console.error(error);
